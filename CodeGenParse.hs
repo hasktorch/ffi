a337df457e484d2d5ba5fba1672a5f1db6ed5d93
austinvhuang <austinh@alum.mit.edu>
2017-09-16 19:54:20 -0400

    move codegen to separate directory, target python core library instead of lua

---
+++ CodeGenParse.hs
@@ -0,0 +1,335 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module CodeGenParse (
+  Parser,
+  thFile,
+  THType(..),
+  THArg(..),
+  THFunction(..)
+  ) where
+
+import Control.Monad (void)
+import Data.Maybe
+import Data.Void
+import Data.Text
+import Data.Text as T
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Expr
+import qualified Text.Megaparsec.Char.Lexer as L
+import Prelude as P
+import Text.Show.Pretty
+
+-- ----------------------------------------
+-- Parsed types
+-- ----------------------------------------
+
+data THType =
+  THVoidPtr
+  | THVoid
+  | THDescBuff
+  | THTensorPtrPtr
+
+  | THTensorPtr
+  | THByteTensorPtr
+  | THLongTensorPtr
+  | THShortTensorPtr
+  | THIntTensorPtr
+  | THHalfTensorPtr
+  | THCharTensorPtr
+  | THDoubleTensorPtr
+  | THFloatTensorPtr
+
+  | THGeneratorPtr
+
+  | THStoragePtr
+  | THByteStoragePtr
+  | THCharStoragePtr
+  | THShortStoragePtr
+  | THIntStoragePtr
+  | THLongStoragePtr
+  | THHalfStoragePtr
+  | THFloatStoragePtr
+  | THDoubleStoragePtr
+
+  | THAllocatorPtr
+  | THPtrDiff
+
+  | THDouble
+  | THLongPtr
+  | THLong
+  | THIntPtr
+  | THInt
+  | THSize
+  | THCharPtr
+  | THChar
+  | THRealPtr
+  | THReal
+  | THAccRealPtr
+  | THAccReal
+  deriving (Eq, Show)
+
+data THArg = THArg {
+  thArgType :: THType,
+  thArgName :: Text -- some signatures, e.g. Storage.h have no variable name
+  } deriving (Eq, Show)
+
+data THFunction = THFunction {
+                funName :: Text,
+                funArgs :: [THArg],
+                funReturn :: THType
+                } deriving (Eq, Show)
+
+type Parser = Parsec Void String
+
+-- ----------------------------------------
+-- File parser for TH templated header files
+-- ----------------------------------------
+
+thPtr :: Parser Char
+thPtr = char '*'
+
+thVoidPtr :: Parser THType
+thVoidPtr = (string "void *" <|> string "void*") >> pure THVoidPtr
+
+thVoid :: Parser THType
+thVoid = string "void" >> pure THVoid
+
+thDouble :: Parser THType
+thDouble = string "double" >> pure THDouble
+
+thDescBuff :: Parser THType
+thDescBuff = string "THDescBuff" >> pure THDescBuff
+
+{- Tensor types -}
+
+thTensorPtr :: Parser THType
+thTensorPtr = string "THTensor" >> space >> thPtr >> pure THTensorPtr
+
+thTensorPtrPtr :: Parser THType
+-- thTensorPtrPtr = string "THTensor" >> space >> (count 2 thPtr) >> pure THTensorPtrPtr
+thTensorPtrPtr = string "THTensor **" >> pure THTensorPtrPtr
+-- TODO : clean up pointer matching
+
+thByteTensorPtr :: Parser THType
+thByteTensorPtr = string "THByteTensor" >> space >> thPtr >> pure THByteTensorPtr
+
+thShortTensorPtr :: Parser THType
+thShortTensorPtr = string "THShortTensor" >> space >> thPtr >> pure THShortTensorPtr
+
+thIntTensorPtr :: Parser THType
+thIntTensorPtr = string "THIntTensor" >> space >> thPtr >> pure THIntTensorPtr
+
+thLongTensorPtr :: Parser THType
+thLongTensorPtr = string "THLongTensor" >> space >> thPtr >> pure THLongTensorPtr
+
+thHalfTensorPtr :: Parser THType
+thHalfTensorPtr = string "THHalfTensor" >> space >> thPtr >> pure THHalfTensorPtr
+
+thCharTensorPtr :: Parser THType
+thCharTensorPtr = string "THCharTensor" >> space >> thPtr >> pure THCharTensorPtr
+
+thDoubleTensorPtr :: Parser THType
+thDoubleTensorPtr = string "THDoubleTensor" >> space >> thPtr >> pure THDoubleTensorPtr
+
+thFloatTensorPtr :: Parser THType
+thFloatTensorPtr = string "THFloatTensor" >> space >> thPtr >> pure THFloatTensorPtr
+
+{- Storage -}
+
+thStoragePtr :: Parser THType
+thStoragePtr = (string "THStorage *" <|> string "THStorage*") >> pure THStoragePtr
+
+thByteStoragePtr :: Parser THType
+thByteStoragePtr = (string "THByteStorage *" <|> string "THByteStorage*")
+  >> space >> pure THByteStoragePtr
+
+thCharStoragePtr :: Parser THType
+thCharStoragePtr = (string "THCharStorage *" <|> string "THCharStorage*")
+  >> space >> pure THCharStoragePtr
+
+thShortStoragePtr :: Parser THType
+thShortStoragePtr = (string "THShortStorage *" <|> string "THShortStorage*")
+  >> space >> pure THShortStoragePtr
+
+thIntStoragePtr :: Parser THType
+thIntStoragePtr = (string "THIntStorage *" <|> string "THIntStorage*")
+  >> space >> pure THIntStoragePtr
+
+thHalfStoragePtr :: Parser THType
+thHalfStoragePtr = (string "THHalfStorage *" <|> string "THHalfStorage*")
+  >> space >> pure THHalfStoragePtr
+
+
+thLongStoragePtr :: Parser THType
+-- thLongStoragePtr = string "THLongStorage" >> space >> thPtr >> pure THStoragePtr
+thLongStoragePtr = (string "THLongStorage *" <|> string "THLongStorage*")
+  >> space >> pure THLongStoragePtr
+
+thFloatStoragePtr :: Parser THType
+thFloatStoragePtr = (string "THFloatStorage *" <|> string "THFloatStorage*")
+  >> space >> pure THFloatStoragePtr
+
+thDoubleStoragePtr :: Parser THType
+thDoubleStoragePtr = (string "THDoubleStorage *" <|> string "THDoubleStorage*")
+  >> space >> pure THDoubleStoragePtr
+
+{- Other -}
+
+thGeneratorPtr :: Parser THType
+thGeneratorPtr = string "THGenerator" >> space >> thPtr >> pure THGeneratorPtr
+
+thLongAllocatorPtr :: Parser THType
+thLongAllocatorPtr = (string "THAllocator *" <|> string "THAllocator*")
+  >> space >> pure THAllocatorPtr
+
+thPtrDiff :: Parser THType
+thPtrDiff = string "ptrdiff_t" >> pure THStoragePtr
+
+thLongPtr :: Parser THType
+thLongPtr = string "long *" >> pure THLongPtr
+-- TODO : clean up pointer matching
+
+thLong :: Parser THType
+thLong = string "long" >> pure THLong
+
+thIntPtr :: Parser THType
+-- thIntPtr = string "int" >> space >> thPtr >> pure THIntPtr
+thIntPtr = (string "int *" <|> string "int* ") >> pure THIntPtr
+
+thInt :: Parser THType
+thInt = string "int" >> pure THInt
+
+thSize :: Parser THType
+thSize = string "size_t" >> pure THSize
+
+thCharPtr :: Parser THType
+thCharPtr = (string "char*" <|> string "char *") >> pure THChar
+
+thChar :: Parser THType
+thChar = string "char" >> pure THChar
+
+thRealPtr :: Parser THType
+thRealPtr = (string "real *" <|> string "real* ") >> pure THRealPtr
+-- TODO : clean up pointer matching
+
+thReal :: Parser THType
+thReal = string "real" >> pure THReal
+
+thAccReal :: Parser THType
+thAccReal = string "accreal" >> pure THAccReal
+
+thAccRealPtr :: Parser THType
+thAccRealPtr = string "accreal *" >> pure THAccRealPtr
+
+thType = do
+  ((string "const " >> pure ())
+    <|> (string "struct " >> pure ()) -- See THStorageCopy.h
+    <|> space)
+  (
+    -- pointers take precedence in parsing
+    thVoidPtr
+    <|> thVoid
+    <|> thDescBuff
+
+    <|> thTensorPtrPtr
+    <|> thTensorPtr
+
+    <|> thByteTensorPtr
+    <|> thShortTensorPtr
+    <|> thIntTensorPtr
+    <|> thLongTensorPtr
+    <|> thHalfTensorPtr
+    <|> thCharTensorPtr
+    <|> thDoubleTensorPtr
+    <|> thFloatTensorPtr
+
+    <|> thGeneratorPtr
+
+    <|> thStoragePtr
+    <|> thByteStoragePtr
+    <|> thCharStoragePtr
+    <|> thShortStoragePtr
+    <|> thIntStoragePtr
+    <|> thLongStoragePtr
+    <|> thHalfStoragePtr
+    <|> thFloatStoragePtr
+    <|> thDoubleStoragePtr
+
+    <|> thLongAllocatorPtr
+    <|> thDouble
+    <|> thPtrDiff
+    <|> thLongPtr
+    <|> thLong
+    <|> thIntPtr
+    <|> thInt
+    <|> thSize
+    <|> thCharPtr
+    <|> thChar
+    <|> thRealPtr
+    <|> thReal
+    <|> thAccRealPtr
+    <|> thAccReal
+    )
+
+-- Landmarks
+
+thAPI :: Parser String
+thAPI = string "TH_API"
+
+thSemicolon :: Parser Char
+thSemicolon = char ';'
+
+-- Function signatures
+
+thFunctionArgVoid = do
+  arg <- thVoid
+  space
+  char ')' :: Parser Char -- TODO move this outside
+  pure $ THArg THVoid ""
+
+thFunctionArgNamed = do
+  argType <- thType
+  space
+  -- e.g. declaration sometimes has no variable name - eg Storage.h
+  argName <- (some (alphaNumChar <|> char '_')) <|> string ""
+  space
+  (char ',' :: Parser Char) <|> (char ')' :: Parser Char)
+  space
+  pure $ THArg argType (T.pack argName)
+
+thFunctionArg = thFunctionArgNamed <|> thFunctionArgVoid
+
+thFunctionArgs = do
+  char '(' :: Parser Char
+  functionArgs <- some thFunctionArg
+  -- close paren consumed by last thFunctionArg (TODO - clean this up)
+  pure functionArgs
+
+thFunctionPrefixes = string "THTensor_("
+                     <|> string "THBlas_("
+                     <|> string "THLapack_("
+                     <|> string "THStorage_("
+                     <|> string "THVector_("
+
+thFunctionTemplate = do
+  thAPI >> space
+  funRet <- thType
+  space
+  thFunctionPrefixes
+  funName <- some (alphaNumChar <|> char '_')
+  space
+  string ")"
+  space
+  funArgs <- thFunctionArgs
+  thSemicolon
+  pure $ Just $ THFunction (T.pack funName) funArgs funRet
+
+-- TODO - exclude thAPI so it doesn't parse if TH_API is invalid
+thSkip = do
+  eol <|> (some (notChar '\n') >> eol)
+  pure Nothing
+
+thItem = thFunctionTemplate <|> thSkip -- ordering is important
+
+thFile = some thItem
---
+++ CodeGenRender.hs
@@ -0,0 +1,384 @@
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Main where
+
+import Control.Monad (void)
+import Data.Monoid ((<>))
+import Data.Maybe
+import Data.Void
+import Data.Text
+import Data.Text as T
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Expr
+import qualified Text.Megaparsec.Char.Lexer as L
+import Prelude as P
+import Text.Show.Pretty
+
+import CodeGenParse
+import CodeGenTypes
+
+-- ----------------------------------------
+-- Types for rendering output
+-- ----------------------------------------
+
+data HModule = HModule {
+  modHeader :: FilePath,
+  modPrefix :: Text,
+  modTypeTemplate :: TemplateType,
+  modSuffix :: Text,
+  modFileSuffix :: Text,
+  modExtensions :: [Text],
+  modImports :: [Text],
+  modTypeDefs :: [(Text, Text)],
+  modBindings :: [THFunction]
+  } deriving Show
+
+data TypeCategory = ReturnValue | FunctionParam
+
+-- ----------------------------------------
+-- Rendering
+-- ----------------------------------------
+
+makePrefix :: Text -> Text
+makePrefix templateType = "TH" <> templateType <> "Tensor"
+
+renderCType :: THType -> Text
+renderCType THVoid = "void"
+renderCType THDescBuff = "THDescBuff"
+renderCType THTensorPtr = "THTensor *"
+renderCType THTensorPtrPtr = "THTensor **"
+renderCType THByteTensorPtr = "THByteTensor *"
+renderCType THLongTensorPtr = "THLongTensor *"
+renderCType THDoubleTensorPtr = "THDoubleTensor *"
+renderCType THFloatTensorPtr = "THFloatTensor *"
+renderCType THGeneratorPtr = "THGenerator *"
+renderCType THStoragePtr = "THStorage *"
+renderCType THLongStoragePtr = "THLongStorage *"
+renderCType THPtrDiff = "ptrdiff_t"
+renderCType THLongPtr = "long *"
+renderCType THLong = "long"
+renderCType THIntPtr = "int *"
+renderCType THInt = "int"
+renderCType THSize = "size_t"
+renderCType THCharPtr = "char *"
+renderCType THChar = "char"
+renderCType THRealPtr = "real *"
+renderCType THReal = "real"
+renderCType THAccRealPtr = "accreal *"
+renderCType THAccReal = "accreal"
+
+renderHaskellType :: TypeCategory -> TemplateType -> THType -> Maybe Text
+
+renderHaskellType _ templateType THVoidPtr = Just "Ptr ()"
+
+renderHaskellType typeCat templateType THVoid =
+  case typeCat of
+    ReturnValue -> Just "IO ()"
+    FunctionParam -> Nothing
+
+renderHaskellType _ _ THDescBuff = Just "CTHDescBuff"
+
+{- Tensor -}
+
+renderHaskellType typeCat templateType THTensorPtrPtr = case typeCat of
+  ReturnValue -> Just $ "IO (Ptr (Ptr CTH" <> type2SpliceReal templateType <> "Tensor))"
+  FunctionParam -> Just $ "Ptr (Ptr CTH" <> type2SpliceReal templateType <> "Tensor)"
+
+renderHaskellType typeCat templateType THTensorPtr = case typeCat of
+  ReturnValue -> Just $ "IO (Ptr CTH" <> type2SpliceReal templateType <> "Tensor)"
+  FunctionParam -> Just $ "(Ptr CTH" <> type2SpliceReal templateType <> "Tensor)"
+
+renderHaskellType typeCat templateType THByteTensorPtr = case typeCat of
+  ReturnValue -> Just "IO (Ptr CTHByteTensor)"
+  FunctionParam -> Just "Ptr CTHByteTensor"
+
+renderHaskellType typeCat templateType THCharTensorPtr = case typeCat of
+  ReturnValue -> Just "IO (Ptr CTHCharTensor)"
+  FunctionParam -> Just "Ptr CTHCharTensor"
+
+renderHaskellType typeCat templateType THShortTensorPtr = case typeCat of
+  ReturnValue -> Just "IO (Ptr CTHShortTensor)"
+  FunctionParam -> Just "Ptr CTHShortTensor"
+
+renderHaskellType typeCat templateType THHalfTensorPtr = case typeCat of
+  ReturnValue -> Just "IO (Ptr CTHHalfTensor)"
+  FunctionParam -> Just "Ptr CTHHalfTensor"
+
+renderHaskellType typeCat templateType THIntTensorPtr = case typeCat of
+  ReturnValue -> Just "IO (Ptr CTHIntTensor)"
+  FunctionParam -> Just "Ptr CTHIntTensor"
+
+renderHaskellType typeCat templateType THLongTensorPtr = case typeCat of
+  ReturnValue -> Just "IO (Ptr CTHLongTensor)"
+  FunctionParam -> Just "Ptr CTHLongTensor"
+
+renderHaskellType typeCat templateType THFloatTensorPtr = case typeCat of
+  ReturnValue -> Just "IO (Ptr CTHFloatTensor)"
+  FunctionParam -> Just "Ptr CTHFloatTensor"
+
+renderHaskellType typeCat templateType THDoubleTensorPtr = case typeCat of
+  ReturnValue -> Just "IO (Ptr CTHDoubleTensor)"
+  FunctionParam -> Just "Ptr CTHDoubleTensor"
+
+{- Storage -}
+
+renderHaskellType typeCat templateType THStoragePtr = case typeCat of
+  ReturnValue -> Just $ "IO (Ptr CTH" <> type2SpliceReal templateType <> "Storage)"
+  FunctionParam -> Just $ "Ptr CTH" <> type2SpliceReal templateType <> "Storage"
+
+renderHaskellType typeCat templateType THByteStoragePtr = case typeCat of
+  ReturnValue -> Just $ "IO (Ptr CTHByteStorage)"
+  FunctionParam -> Just $ "Ptr CTHByteStorage"
+
+renderHaskellType typeCat templateType THShortStoragePtr = case typeCat of
+  ReturnValue -> Just $ "IO (Ptr CTHShortStorage)"
+  FunctionParam -> Just $ "Ptr CTHShortStorage"
+
+renderHaskellType typeCat templateType THIntStoragePtr = case typeCat of
+  ReturnValue -> Just $ "IO (Ptr CTHIntStorage)"
+  FunctionParam -> Just $ "Ptr CTHIntStorage"
+
+renderHaskellType typeCat templateType THLongStoragePtr = case typeCat of
+  ReturnValue -> Just $ "IO (Ptr CTHLongStorage)"
+  FunctionParam -> Just $ "Ptr CTHLongStorage"
+
+renderHaskellType typeCat templateType THHalfStoragePtr = case typeCat of
+  ReturnValue -> Just $ "IO (Ptr CTHHalfStorage)"
+  FunctionParam -> Just $ "Ptr CTHHalfStorage"
+
+renderHaskellType typeCat templateType THCharStoragePtr = case typeCat of
+  ReturnValue -> Just $ "IO (Ptr CTHCharStorage)"
+  FunctionParam -> Just $ "Ptr CTHCharStorage"
+
+renderHaskellType typeCat templateType THFloatStoragePtr = case typeCat of
+  ReturnValue -> Just $ "IO (Ptr CTHFloatStorage)"
+  FunctionParam -> Just $ "Ptr CTHFloatStorage"
+
+renderHaskellType typeCat templateType THDoubleStoragePtr = case typeCat of
+  ReturnValue -> Just $ "IO (Ptr CTHDoubleStorage)"
+  FunctionParam -> Just $ "Ptr CTHDoubleStorage"
+
+{- Other -}
+
+renderHaskellType typeCat templateType THGeneratorPtr = case typeCat of
+  ReturnValue -> Just ("IO (Ptr CTHGenerator") -- concrete type found in TensorMat)h
+  FunctionParam -> Just ("Ptr CTHGenerator") -- concrete type found in TensorMath
+
+renderHaskellType typeCat templateType THAllocatorPtr = case typeCat of
+  ReturnValue -> Just $ "IO (CTHAllocatorPtr)"
+  FunctionParam -> Just $ "CTHAllocatorPtr"
+
+renderHaskellType _ templateType THDouble =
+  Just "CDouble" -- added from TensorRandom
+
+renderHaskellType typeCat templateType THPtrDiff = case typeCat of
+  ReturnValue -> Just $ "IO (CTH" <> type2SpliceReal templateType <> "PtrDiff)"
+  FunctionParam -> Just $ "CTH" <> type2SpliceReal templateType <> "PtrDiff"
+  -- TODO check if it's appropriate to splice here
+
+renderHaskellType typeCat templateType THLongPtr = case typeCat of
+  ReturnValue -> Just "IO (Ptr CLong)"
+  FunctionParam -> Just "Ptr CLong"
+
+renderHaskellType _ templateType THLong =
+  Just "CLong"
+
+renderHaskellType typeCat templateType THIntPtr = case typeCat of
+  ReturnValue -> Just "IO (CIntPtr)"
+  FunctionParam -> Just "CIntPtr"
+
+renderHaskellType _ templateType THInt =
+  Just "CInt"
+
+renderHaskellType _ templateType THSize =
+  Just "CSize"
+
+renderHaskellType typeCat templateType THCharPtr = case typeCat of
+  ReturnValue -> Just "IO (Ptr CChar)"
+  FunctionParam -> Just "Ptr CChar"
+
+renderHaskellType _ templateType THChar =
+  Just "CChar"
+
+renderHaskellType typeCat templateType THRealPtr = case typeCat of
+  ReturnValue -> Just $ "IO (Ptr " <> realtype2Haskell templateType <> ")"
+  FunctionParam -> Just $ "Ptr " <> realtype2Haskell templateType
+
+renderHaskellType _ templateType THReal =
+  Just $ realtype2Haskell templateType
+
+renderHaskellType typeCat templateType THAccRealPtr = case typeCat of
+  ReturnValue -> Just $ "IO (Ptr " <> accrealtype2Haskell templateType <> ")"
+  FunctionParam -> Just $ "Ptr " <> accrealtype2Haskell templateType
+
+renderHaskellType _ templateType THAccReal =
+  Just $ accrealtype2Haskell templateType
+
+renderExtension :: Text -> Text
+renderExtension extension = "{-# LANGUAGE " <> extension <> "#-}"
+
+renderExtensions :: [Text] -> Text
+renderExtensions extensions =
+  (T.intercalate "\n" (renderExtension <$> extensions)) <> "\n\n"
+
+renderModuleName :: HModule -> Text
+renderModuleName HModule{..} =
+  modPrefix <> (type2SpliceReal modTypeTemplate) <> modFileSuffix
+
+renderModule :: HModule -> Text
+renderModule moduleSpec =
+  "module " <> (renderModuleName moduleSpec)
+
+renderExports :: [Text] -> Text
+renderExports exports = (" (\n    "
+                         <> (T.intercalate ",\n    " exports)
+                         <> ") where\n\n")
+
+renderImports :: [Text] -> Text
+renderImports imports = (T.intercalate "\n" (singleimport <$> imports)) <> "\n\n"
+  where singleimport x = "import " <> x
+
+renderFunName :: Text -> Text -> Text
+renderFunName prefix name = prefix <> "_" <> name
+
+renderFunSig :: FilePath -> TemplateType -> (Text, THType, [THArg]) -> Text
+renderFunSig headerFile modTypeTemplate (name, retType, args) =
+  (
+   "-- |c_" <> name <> " : "
+   <> (T.intercalate " " nameSignature) <> " -> " <> (renderCType retType) <> "\n"
+   <> "foreign import ccall \"" <> T.pack headerFile <> " " <> name <> "\"\n"
+   <> "  c_" <> name <> " :: "
+   <> (T.intercalate " -> " typeSignatureClean)
+    -- TODO : fromJust shouldn't fail but still clean this up so it's not unsafe
+   <> retArrow <> fromJust (renderHaskellType ReturnValue modTypeTemplate retType)
+  )
+  where
+    typeVals = thArgType <$> args
+    typeSignature = renderHaskellType FunctionParam modTypeTemplate <$> typeVals
+    typeSignatureClean = catMaybes typeSignature
+    numArgs = P.length typeSignatureClean
+    retArrow = if numArgs == 0 then "" else " -> "
+    nameSignature = thArgName <$> args
+
+renderFunctions :: HModule -> Text
+renderFunctions moduleSpec@HModule{..} =
+  -- iteration over all functions
+  intercalate "\n\n" ((renderFunSig modHeader typeTemplate)
+                      <$> (P.zip3 funNames retTypes args) )
+  where
+    modulePrefix = (renderModuleName moduleSpec) <> "_"
+    funNames = (mappend modulePrefix) <$> funName <$> modBindings
+    retTypes = funReturn <$> modBindings
+    args = funArgs <$> modBindings
+    typeTemplate = modTypeTemplate
+
+renderAll :: HModule -> Text
+renderAll spec =
+  (renderExtensions (modExtensions spec)
+   <> renderModule spec
+   <> renderExports exportFunctions
+   <> renderImports (modImports spec)
+   <> renderFunctions spec)
+  where
+    prefix = makePrefix . type2SpliceReal . modTypeTemplate $ spec
+    bindings = modBindings spec
+    exportFunctions =
+      (renderFunName ("c_" <> renderModuleName spec)
+       <$> (fmap funName (modBindings spec)))
+
+-- ----------------------------------------
+-- Execution
+-- ----------------------------------------
+
+parseFromFile p file = runParser p file <$> readFile file
+
+cleanList :: Either (ParseError Char Void) [Maybe THFunction] -> [THFunction]
+cleanList (Left _) = []
+cleanList (Right lst) = fromJust <$> (P.filter f lst)
+  where
+    f Nothing = False
+    f (Just _) = True
+
+makeModule modHeader modSuffix modFileSuffix typeTemplate bindings =
+   HModule {
+        modHeader = modHeader,
+        modPrefix = "TH",
+        modTypeTemplate = typeTemplate,
+        modSuffix = modSuffix,
+        modFileSuffix = modFileSuffix,
+        modExtensions = ["ForeignFunctionInterface"],
+        modImports = ["Foreign", "Foreign.C.Types", "THTypes"],
+        modTypeDefs = [],
+        modBindings = bindings
+  }
+
+parseFile file = do
+  putStrLn $ "\nParsing " ++ file ++ " ... "
+  res <- parseFromFile thFile file
+  pure $ cleanList res
+
+renderCHeader templateType parsedBindings makeConfig = do
+  putStrLn $ "Writing " <> T.unpack filename
+  writeFile ("./th-bindings/" ++ T.unpack filename) (T.unpack . renderAll $ modSpec)
+  where modSpec = makeConfig templateType parsedBindings
+        filename = (renderModuleName modSpec) <> ".hs"
+
+runPipeline headerPath makeModuleConfig = do
+  parsedBindings <- parseFile headerPath
+  putStrLn $ "First signature:"
+  putStrLn $ ppShow (P.take 1 parsedBindings)
+  mapM_ (\x -> renderCHeader x parsedBindings makeModuleConfig) genTypes
+  putStrLn $ "Number of functions generated: " ++
+    (show $ P.length genTypes * P.length parsedBindings)
+
+parseFiles :: [(String, TemplateType -> [THFunction] -> HModule)]
+parseFiles =
+  [
+    ("vendor/torch7/lib/TH/generic/THBlas.h",
+     (makeModule "THBlas.h" "Blas" "Blas")),
+    ("vendor/torch7/lib/TH/generic/THLapack.h",
+     (makeModule "THLapack.h" "Lapack" "Lapack")),
+    ("vendor/torch7/lib/TH/generic/THStorage.h",
+     (makeModule "THStorage.h" "Storage" "Storage")),
+    ("vendor/torch7/lib/TH/generic/THStorageCopy.h",
+     (makeModule "THStorageCopy.h" "Storage" "StorageCopy")),
+    ("vendor/torch7/lib/TH/generic/THTensor.h",
+     (makeModule "THTensor.h" "Tensor" "Tensor")),
+    ("vendor/torch7/lib/TH/generic/THTensorConv.h",
+     (makeModule "THTensorConv.h" "Tensor" "TensorConv")),
+    ("vendor/torch7/lib/TH/generic/THTensorCopy.h",
+     (makeModule "THTensorCopy.h" "Tensor" "TensorCopy")),
+    ("vendor/torch7/lib/TH/generic/THTensorLapack.h",
+     (makeModule "THTensorLapack.h" "Tensor" "TensorLapack")),
+    ("vendor/torch7/lib/TH/generic/THTensorMath.h",
+     (makeModule "THTensorMath.h" "Tensor" "TensorMath")),
+    ("vendor/torch7/lib/TH/generic/THTensorRandom.h",
+     (makeModule "THTensorRandom.h" "Tensor" "TensorRandom")),
+    ("vendor/torch7/lib/TH/generic/THVector.h",
+     (makeModule "THVector.h" "Vector" "Vector"))
+  ]
+
+makeReExports = do
+  putStrLn "Re-exported"
+
+testString inp = case (parse thFile "" inp) of
+  Left err -> putStrLn (parseErrorPretty err)
+  Right val -> putStrLn $ (ppShow val)
+
+test1 = do
+  testString ex1
+  where
+    ex1 = "skip this garbage line line\n" <>
+     "TH_API void THTensor_(setFlag)(THTensor *self,const char flag);" <>
+     "another garbage line ( )@#R @# 324 32"
+
+test2 = runPipeline "vendor/check.h"
+  (makeModule "THStorage.h" "Storage" "Storage")
+
+main :: IO ()
+main = do
+  mapM_ (\(file, spec) -> runPipeline file spec) parseFiles
+  putStrLn "Done"
