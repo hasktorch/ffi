a183760b60ea8f12b78b2f33e2c0f8e9970c8718
austinvhuang <austinh@alum.mit.edu>
2017-12-20 18:36:52 -0500

    move interface/ into raw/, WIP ad example tweaks, minor cleanup of warnings in codegen/

---
+++ src/types/THTypes.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module THTypes where
+
+import Foreign
+import Foreign.C.String
+import Foreign.C.Types
+import Foreign.Ptr
+import Foreign.Storable
+
+import TorchStructs
+
+type CTHDescBuff = Ptr ()
+type CTHAllocatorPtr = Ptr C'THAllocator
+type CTHGenerator = C'THGenerator
+type CTHFile = ()
+type CTHHalf = CUShort
+
+-- ----------------------------------------
+-- Templated types
+-- ----------------------------------------
+
+{- Byte -}
+
+type CTHByteTensor = C'THByteTensor
+type CTHByteStorage = C'THByteStorage
+
+
+{- Char -}
+
+type CTHCharTensor = C'THCharTensor
+type CTHCharStorage = C'THCharStorage
+
+{- Double -}
+
+type CTHDoubleTensor = C'THDoubleTensor
+type CTHDoubleStorage = C'THDoubleStorage
+
+{- Float -}
+
+type CTHFloatTensor = C'THFloatTensor
+type CTHFloatStorage = C'THFloatStorage
+
+{- Half -}
+
+type CTHHalfTensor = ()
+type CTHHalfStorage = ()
+
+{- Int -}
+
+type CTHIntTensor = C'THIntTensor
+type CTHIntStorage = C'THIntStorage
+
+{- Long -}
+
+type CTHLongTensor = C'THLongTensor
+type CTHLongStorage = C'THLongStorage
+
+{- Short -}
+
+type CTHShortTensor = C'THShortTensor
+type CTHShortStorage = C'THShortStorage
