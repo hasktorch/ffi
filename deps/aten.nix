311bb8ff16eaada4fa6c3ca3a355307b24ba71f9
Sam Stites <sam@stites.io>
2018-10-24 16:41:50 -0400

    add mkl support to aten

--- deps/aten.nix
+++ deps/aten.nix
@@ -1,12 +1,15 @@
 # See: https://gist.github.com/CMCDragonkai/41593d6d20a5f7c01b2e67a221aa0330
 
-{ stdenv, cmake, blas, liblapack, gfortran, lib
-, typing, pyaml, magma
-, cudaSupport ? false, cudatoolkit ? null
+{ stdenv, cmake, gfortran, lib
+, typing, pyaml
+, cudaSupport ? false, cudatoolkit ? null, cudnn ? null, magma ? null
+, mklSupport ? false, blas ? null, liblapack ? null, mkl ? null
 , dev ? false
 }:
 
-assert cudaSupport -> cudatoolkit != null;
+assert cudaSupport -> cudatoolkit != null && cudnn != null;
+assert  mklSupport -> mkl != null;
+assert !mklSupport -> blas != null && liblapack != null;
 
 stdenv.mkDerivation rec {
   name = "hasktorch-aten";
@@ -16,11 +19,10 @@ stdenv.mkDerivation rec {
     cmake
     typing
     pyaml
-    blas
-    liblapack
     gfortran.cc.lib
     ]
-    ++ lib.optionals cudaSupport [cudatoolkit magma];
+    ++ lib.optionals cudaSupport [cudatoolkit magma cudnn]
+    ++ (if mklSupport then [mkl] else [blas liblapack]);
   cmakeFlags = [
     ("-DNO_CUDA=" + (if cudaSupport then "false" else "true"))
     (if dev then "-Wno-dev" else "")
